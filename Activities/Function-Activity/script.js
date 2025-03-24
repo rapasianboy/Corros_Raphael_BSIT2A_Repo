07fc00d435eec42fd2434670f9909d13ff50a526
Your Name <you@example.com>
2025-03-24 01:44:44 -0700

    new version activity functions

---
+++ Activities/Function-Activity/script.js
@@ -0,0 +1,14 @@
+function whatShallWear(temp) {
+    if (temp <50) {
+        console.log("wear a sweater");
+    } else if (temp<60) {
+        console.log("wear a T-shirt" )
+    } else {
+        console.log("wear a jacket ")
+    }
+
+}
+
+whatShallWear(60)
+whatShallWear(50)
+whatShallWear(80)
---
+++ Getting Functional/functional.js
@@ -0,0 +1,45 @@
+
+
+
+var dogName = "Bobby"
+var dogWeight = 30
+if (dogWeight >20) {
+    console.log(dogName + "Says arf arf");
+} else {
+    console.log(dogName + "Says arf arf");
+}
+   
+
+
+var dogName = "Bobby"
+var dogWeight = 30
+if (dogWeight >20) {
+    console.log(dogName + "Says arf arf");
+} else {
+    console.log(dogName + "Says arf arf");
+}
+   
+var dogName = "Bobby"
+var dogWeight = 30
+if (dogWeight >20) {
+    console.log(dogName + "Says arf arf");
+} else {
+    console.log(dogName + "Says arf arf");
+}
+   
+var dogName = "Bobby"
+var dogWeight = 30
+if (dogWeight >20) {
+    console.log(dogName + "Says arf arf");
+} else {
+    console.log(dogName + "Says arf arf");
+
+}
+ 
+function bark(name,weight) {
+    if (weight>20){
+        console.log (Dogname + "says ARF ARF");
+    } else {
+        console.log (Dogname + "says ARF ARF")
+    }
+}
